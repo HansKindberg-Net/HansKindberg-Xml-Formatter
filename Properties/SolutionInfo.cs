43393c36f408144d44c4008974cc98e38b86f6be
Hans Kindberg <hans.kindberg@gmail.com>
2016-11-03 12:11:38 +0100

    Changed version number. Changed installation-target version.

--- Properties/SolutionInfo.cs
+++ Properties/SolutionInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Hans Kindberg - open source")]
 [assembly: AssemblyConfiguration(
 #if DEBUG
-	"Debug"
+	           "Debug"
 #else
 	"Release"
 #endif
-	)]
+           )]
 /*
 	The third position in the version (AssemblyVersion) number is used for the status of the release:
 	0 for alpha (status)
@@ -43,10 +43,10 @@ using System.Runtime.InteropServices;
 */
 #pragma warning disable 436
 
-[assembly: AssemblyFileVersion(SolutionInfo.AssemblyFileVersion)]
-[assembly: AssemblyInformationalVersion(SolutionInfo.AssemblyInformationalVersion)]
-[assembly: AssemblyProduct(AssemblyInfo.AssemblyName + " " + SolutionInfo.AssemblyInformationalVersion)]
-[assembly: AssemblyTitle(AssemblyInfo.AssemblyName + " " + SolutionInfo.AssemblyInformationalVersion)]
+[assembly: AssemblyFileVersion(SolutionInfo.AssemblyVersion + ".0")]
+[assembly: AssemblyInformationalVersion(SolutionInfo.AssemblyVersion + "-alpha")]
+[assembly: AssemblyProduct(AssemblyInfo.AssemblyName)]
+[assembly: AssemblyTitle(AssemblyInfo.AssemblyName)]
 [assembly: AssemblyVersion(SolutionInfo.AssemblyVersion)]
 #pragma warning restore 436
 
@@ -55,24 +55,9 @@ using System.Runtime.InteropServices;
 
 internal static class SolutionInfo
 {
-	// When you increase the majorversion: minorversion = 0, buildnumber = 0, revision = 0, fileversionrevision = 0
-	// When you increase the minorversion: buildnumber = 0, revision = 0, fileversionrevision = 0
-	// When you increase the buildnumber: revision = 0, fileversionrevision = 0
-	// Else - foreach change: revision++, fileersionrevision++
-	// Else - foreach small fix: fileversionrevision++
-	// Note: fileversionrevision should not be less than revision
-
 	#region Fields
 
-	internal const string AssemblyFileVersion = _assemblyBaseVersion + "." + _fileVersionRevision;
-	internal const string AssemblyInformationalVersion = _assemblyBaseVersion + "-alpha-" + _revision;
-	internal const string AssemblyVersion = _assemblyBaseVersion + "." + _revision;
-	private const string _assemblyBaseVersion = _majorVersion + "." + _minorVersion + "." + _buildNumber;
-	private const string _buildNumber = "0";
-	private const string _fileVersionRevision = "0";
-	private const string _majorVersion = "1";
-	private const string _minorVersion = "0";
-	private const string _revision = "0";
+	internal const string AssemblyVersion = "1.1.0";
 
 	#endregion
 }
